3a4267db5b4aeecb397de1fa30e4780a64ac190a
cottonthread <62510349+cottonthread@users.noreply.github.com>
2020-12-24 16:13:44 +0100

    Update auth-hook.py

--- auth-hook.py
+++ auth-hook.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
-
-# DonDominio的DNS验证方法，这样可以直接走通配符放行域名了！
-
-# certbot certonly --logs-dir $HOME/DonDominio/ --work-dir $HOME/DonDominio/ --config-dir $HOME/DonDominio/ --manual --manual-auth-hook $HOME/DonDominio/auth-hook.py --deploy-hook $HOME/DonDominio/deploy-hook.sh -d *.enlace.com.es --preferred-challenges=dns --agree-tos --no-eff-email -m register@disframed.com
-
-# 如果是做更新的话，其实不用加很多参数，因为它会直接读取你在获取证书时的原始配置，也就意味着如果你更换了配置，最好重新走申请新证书的流程，而不是单纯地更新它
-# certbot renew --logs-dir $HOME/DonDominio/ --work-dir $HOME/DonDominio/ --config-dir $HOME/DonDominio/ --manual --manual-auth-hook $HOME/DonDominio/auth-hook.py --deploy-hook $HOME/DonDominio/deploy-hook.sh --preferred-challenges=dns
-
-# certbot revoke --test-cert --delete-after-revoke --non-interactive --cert-path $HOME/DonDominio/archive/enlace.com.es/cert1.pem --key-path $HOME/DonDominio/archive/enlace.com.es/privkey1.pem --logs-dir $HOME/DonDominio/ --work-dir $HOME/DonDominio/ --config-dir $HOME/DonDominio/ -m register@disframed.com && rm -vrf accounts/ archive/ backups/ csr/ keys/ letsencrypt.* live/ renewal* *.pem
-
 import os, json, requests, time
 # 首先把所有的变量收集齐
 apiconf = open("api.conf")
@@ -36,17 +26,17 @@ dataurl1 = {'apiuser': apiuser, 'apipasswd': apipasswd, 'name': name, 'serviceNa
 
 # 获取数据申请结果
 response = json.loads(requests.post(url1, data=dataurl1).text)
-count = 0
-for key, value in response.items():
-	count = count + 1
-	# 只需要回复的前5条记录
-	if count <= 5:
-		print (key + ":", value)
-	else:
-		pass
-if response.get('success') is False:
-	print("Error Ocurred!\n" + "*" * 20)
+if response.get('success') is True:
+	for key, value in response.items():
+		if key == "responseData":
+			pass
+		else:
+			print(key, value)
+else:
+	for key, value in response.items():
+		print(key, value)
 	# 这里是不是要发封邮件啊？
+		
 else:
 	# 获取结果中符合 certbot_domain 的 entityID
 	keys = response.get('responseData').get('dns')
@@ -59,9 +49,8 @@ else:
 			if key.get('value') == certbot_validation:
 				# 如果要更新的值和已有的值一样，那就别管了
 				print("Value you going to update is the same as you recived from Certbot, look:")
-				for dict, value in key.items():
-					print(dict + ":", value)
-				break
+				for item, value in key.items():
+					print(item + ":", value)
 				print ("*" * 20)
 			else:
 				print("Update", key.get('value'), "to", certbot_validation)
@@ -72,36 +61,36 @@ else:
 				# 获取修改结果
 				response = json.loads(requests.post(url2, data=dataurl2).text)
 				print("Done, results:")
-				for key, value in response.items():
-					count = count + 1
-					# 只需要回复的前5条记录
-					if count <= 5:
-						print (key + ":", value)
-					else:
-						pass
-				if response.get('success') is False:
-					print("Error Ocurred!\n" + "*" * 20)
-					# 这里是不是要发封邮件啊？
+				if response.get('success') is True:
+					for key, value in response.items():
+						if key == "responseData":
+							pass
+						else:
+							print(key, value)
+				else:
+					for key, value in response.items():
+						print(key, value)
+						# 这里是不是要发封邮件啊？
 			print ("*" * 20)
 			break
 		# 假如没有相应的TXT项，需要创建的话
 		else:
 			print(name, "is not in", certbot_domain, "so creating...")
 			# 组装修改申请
-			dataurl3 = {'apiuser': apiuser, 'apipasswd': apipasswd, 'name': name, 'serviceName': certbot_domain, 'type': "TXT", 'value': certbot_validation, "ttl": "3600"}
+			dataurl3 = {'apiuser': apiuser, 'apipasswd': apipasswd, 'name': name, 'serviceName': certbot_domain, 'type': "TXT", 'value': certbot_validation, "ttl": 600}
 			count = 0
 			# 获取修改结果
 			response = json.loads(requests.post(url3, data=dataurl3).text)
 			print("Done, results:")
-			for key, value in response.items():
-				count = count + 1
-				# 只需要回复的前5条记录
-				if count <= 5:
-					print (key + ":", value)
-				else:
-					pass
-			if response.get('success') is False:
-				print("Error Ocurred!\n" + "*" * 20)
+			if response.get('success') is True:
+				for key, value in response.items():
+					if key == "responseData":
+						pass
+					else:
+						print(key, value)
+			else:
+				for key, value in response.items():
+					print(key, value)
 				# 这里是不是要发封邮件啊？
 		print ("*" * 20)
 	time.sleep(25)
