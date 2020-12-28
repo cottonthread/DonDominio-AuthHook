da315643ee58863fe19ad093b408b02d5d7aaf25
cottonthread <62510349+cottonthread@users.noreply.github.com>
2020-12-28 01:58:29 +0100

    Update deploy-hook.sh

--- deploy-hook.sh
+++ deploy-hook.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-cp -vf $RENEWED_LINEAGE/fullchain.pem $HOME/DonDominio/"$CERTBOT_DOMAIN"_fullchain.pem
-cp -vf $RENEWED_LINEAGE/privkey.pem $HOME/DonDominio/"$CERTBOT_DOMAIN"_privkey.pem
+cp -vf $RENEWED_LINEAGE/fullchain.pem $HOME/DonDominio-AuthHook/"$CERTBOT_DOMAIN"_fullchain.pem
+cp -vf $RENEWED_LINEAGE/privkey.pem $HOME/DonDominio-AuthHook/"$CERTBOT_DOMAIN"_privkey.pem
